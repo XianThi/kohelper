45cffa86c2faf5a43b8fd4f9d29b4b22557b4b0d
Abdulsamet SAYLIK <rexox@hotmail.de>
2023-02-18 19:40:21 +0300

    Update kohelper.py

--- kohelper.py
+++ kohelper.py
@@ -2,40 +2,42 @@ import requests
 from python_imagesearch.imagesearch import *
 import win32gui
 from time import sleep
+from PIL import ImageGrab
 import ocr
 
 class KoHelper:
     def __init__(self):
         self.found_window = False
-        self.chat_id = -651913034
+        self.chat_id = -644166565
         self.token = "6071564266:AAHjzBD5Wk3_Ot7AtAg7tcfCrrRpkks59TQ"
         self.api = "https://api.telegram.org/bot"
-        self.bot_loop()
         self.disconnect = False
+        self.getUpdates()
+        self.bot_loop()
+        
     def _input(self,message, input_type=str):
         while True:
             try:
                 return input_type (input(message))
             except:
                 pass
-    def imagesearch_count(image, precision=0.9):
-        img_rgb = pyautogui.screenshot()
-        if is_retina:
-            img_rgb.thumbnail((round(img_rgb.size[0] * 0.5), round(img_rgb.size[1] * 0.5)))
-        img_rgb = np.array(img_rgb)
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-        template = cv2.imread(image, 0)
-        w, h = template.shape[::-1]
-        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-        loc = np.where(res >= precision)
-        count = []
-        for pt in zip(*loc[::-1]):  # Swap columns and rows
-            # cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2) // Uncomment to draw boxes around found occurrences
-            count .append((pt[0], pt[1]))
-        # cv2.imwrite('result.png', img_rgb) // Uncomment to write output image with boxes drawn around occurrences
-        return count
+    def getUpdates(self):
+        print("\n[!!!] Lütfen kanalınızın ID'sini güncelleyiniz.\r\n")
+        print("\n[!!!] KANAL LISTESI [!!!!]")
+        url = f"{self.api}{self.token}/getUpdates"
+        json = requests.get(url).json()
+        for i in json["result"] :
+            if i.__contains__("message"):
+                if i["message"]["chat"].__contains__("title"):
+                    kanal = i["message"]["chat"]["title"]
+                else:
+                    kanal = i["message"]["chat"]["username"]
+                kanal_id = i["message"]["chat"]["id"]
+                print (f"{kanal} - {kanal_id}")
+        print("\r\n\r\n")
+    
     def get_name(self,x,y,width,height):
-        screen_capture = ImageGrab.grab(bbox=x,y,x+width,y+height)
+        screen_capture = ImageGrab.grab(bbox=(x,y,x+width,y+height))
         name: str = ocr.get_text_from_image(image=screen_capture)
         return name
     def callback(self,hwnd, extra) -> None:  # pylint: disable=unused-argument
@@ -71,7 +73,7 @@ class KoHelper:
         print("\n[!] Pencere bulundu. Bot baslatılıyor lütfen pencereye geçiş yapınız.")
         sleep(2)
         while not self.disconnect:
-            dead_cnt = self.imagesearch_count("hpbar.png")
+            dead_cnt = imagesearch_count_ex("./images/hpbar.png")
             if len(dead_cnt) > 0:
                 x_count_list = []
                 y_count_list = []
@@ -80,7 +82,7 @@ class KoHelper:
                     x_count_list.append(x_count)
                     y_count_list.append(y_count)
             for i in range(0,len(dead_cnt)):
-                name = self.get_name(self,x_count_list[i]+16,y_count_list[i]+25,95,14)
+                name = self.get_name(x_count_list[i]+16,y_count_list[i]+25,95,14)
                 mesaj = f"{name} öldü. Lütfen çarı diriltin."
                 self.sendMessage(mesaj)
             warrior_cnt = self.search(tip,"warrior")
