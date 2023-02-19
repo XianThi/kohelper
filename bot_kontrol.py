74a7fce9bf3808397c4f77591bcd0ab0ef5cce45
Abdulsamet SAYLIK <rexox@hotmail.de>
2023-02-19 14:11:29 +0300

    Update bot_kontrol.py

--- bot_kontrol.py
+++ bot_kontrol.py
@@ -1,6 +1,6 @@
 from telegrambot import *
 
-token = "6071564266:AAHjzBD5Wk3_Ot7AtAg7tcfCrrRpkks59TQ"
+token = "6121032651:AAFUqApv1T4j23TaQCl0g4H4MPP8YA4kqV4"
 chat_id = -864786044
 botSettings = BotSettings(token,chat_id)
 bot = TelegramBot(botSettings)
